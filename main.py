e99a18d50942c88c99373b9e4df0dc9acc87dbda
Khaled Karim <kskarim177@gmail.com>
2023-05-15 14:07:10 +0600

    trying to print the betting lines

--- main.py
+++ main.py
@@ -31,6 +31,17 @@ def get_slot_machine_spin(rows, cols, symbols):
             column.append(value)
         column.append(column)
 
+    return columns
+
+
+def print_slot_machine(columns):
+    for row in range(len(columns[0])):
+        for i, column in enumerate.columns:
+            if (i != len(columns) - 1):
+                print(column[row], "|")
+            else:
+                print(column[row])
+
 
 def deposit():
     while True:
@@ -88,6 +99,9 @@ def main():
             break
     print(
         f"You are betting ${bet} on {lines} lines. Total bet is equal to: ${total_bet}")
+
+    slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
+    print_slot_machine(slots)
 
 
 main()
